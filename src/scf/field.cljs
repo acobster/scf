d59606a60cdcf6c532997e3dde45e28ef5310265
Coby Tamayo <acobster@gmail.com>
2019-09-27 09:41:09 -0700

    wrap-in fn

--- src/scf/field.cljs
+++ src/scf/field.cljs
@@ -10,3 +10,11 @@
         (string/split v #"[-_]")  ; ["field" "name"]
         (map string/capitalize v) ; ["Field" "Name"]
         (string/join " " v))))    ; "Field Name"
+
+(defn wrap-in [html path]
+  "Takes an element and a path, and returns the element wrapped in nested
+  elements, each of which is one \"step\" in path"
+  (cond
+    (keyword? path) [path html]
+    (fn? path) (path html)
+    :else (reduce (fn [wrapped wrapper] [wrapper wrapped]) html (reverse path))))
--- src/scf/field_test.cljs
+++ src/scf/field_test.cljs
@@ -8,6 +8,21 @@
   (testing "With a label specified"
     (is (= "Hello, World!" (subject/label {:name :goodbye
                                            :label "Hello, World!"}))))
-
   (testing "Without a label specified"
     (is (= "Asdf Qwerty Foo" (subject/label {:name :asdf-qwerty_foo})))))
+
+(deftest wrap-in
+
+  (testing "With a keyword path"
+    (is (= [:div#wrapper [:a "Hello, World!"]]
+           (subject/wrap-in [:a "Hello, World!"] :div#wrapper))))
+
+
+  (testing "With a vector path"
+    (is (= [:div#one [:p.two [:span#three [:a "Hello, World!"]]]]
+           (subject/wrap-in [:a "Hello, World!"] [:div#one :p.two :span#three]))))
+
+  (testing "With a function path"
+    (is (= [:span.dynamic [:a "Hello, World!"]]
+           (subject/wrap-in [:a "Hello, World!"] (fn [elem]
+                                                   [:span.dynamic elem]))))))
